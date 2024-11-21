b0adeed29763703b3faba2b9ebc6680da889c7ce
KolbazovFil <mr.kolbazov@yandex.ru>
2024-11-21 16:29:19 +0300

    2

--- Program.cs
+++ Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _15PR_Kolbazov_RPM
 {
@@ -34,6 +31,18 @@ namespace _15PR_Kolbazov_RPM
             light.Off();
         }
     }
+    public class LightSettingsCommand : Command
+    {
+        private Light light;
+        public LightSettingsCommand(Light light)
+        {
+            this.light = light;
+        }
+        public override void Execute()
+        {
+            light.Settings();
+        }
+    }
     public class AirConditionerOnCommand : Command
     {
         private AirConditioner airConditioner;
@@ -58,10 +67,22 @@ namespace _15PR_Kolbazov_RPM
             airConditioner.Off();
         }
     }
+    public class AirConditionerSettingsCommand : Command
+    {
+        private AirConditioner airConditioner;
+        public AirConditionerSettingsCommand(AirConditioner airConditioner)
+        {
+            this.airConditioner = airConditioner;
+        }
+        public override void Execute()
+        {
+            airConditioner.Settings();
+        }
+    }
     public class TVOnCommand : Command
     {
         private TV tv;
-        public TVOnCommand (TV tv)
+        public TVOnCommand(TV tv)
         {
             this.tv = tv;
         }
@@ -82,6 +103,19 @@ namespace _15PR_Kolbazov_RPM
             tv.Off();
         }
     }
+    public class TVSettingsCommand : Command
+    {
+        private TV tv;
+        public TVSettingsCommand(TV tv)
+        {
+            this.tv = tv;
+        }
+        public override void Execute()
+        {
+            tv.Settings();
+        }
+
+    }
     public class Light
     {
         public void On()
@@ -94,7 +128,7 @@ namespace _15PR_Kolbazov_RPM
         }
         public void Settings()
         {
-            Console.WriteLine("Изменение параметров!");
+            Console.WriteLine("Изменение параметров света!");
         }
     }
     public class AirConditioner
@@ -109,7 +143,7 @@ namespace _15PR_Kolbazov_RPM
         }
         public void Settings()
         {
-            Console.WriteLine("Изменение параметров!");
+            Console.WriteLine("Изменение параметров кондиционера!");
         }
     }
     public class TV
@@ -124,7 +158,7 @@ namespace _15PR_Kolbazov_RPM
         }
         public void Settings()
         {
-            Console.WriteLine("Изменение параметров!");
+            Console.WriteLine("Изменение параметров телевизора!");
         }
     }
     public class RemoteControl
@@ -140,50 +174,350 @@ namespace _15PR_Kolbazov_RPM
         }
     }
     // --------------------------------------------------------------------------
+    public abstract class TaxCalculationStrategy
+    {
+        public abstract decimal CalculateTax(decimal income);
+    }
+    public class ProgressiveTaxCalculationStrategy : TaxCalculationStrategy
+    {
+        public override decimal CalculateTax(decimal income)
+        {
+            if (income <= 10000)
+            {
+                return income * 0.1m; // 10% для дохода до 10,000
+            }
+            else if (income <= 30000)
+            {
+                return 1000 + (income - 10000) * 0.2m; // 20% для дохода от 10,001 до 30,000
+            }
+            else
+            {
+                return 5000 + (income - 30000) * 0.3m; // 30% для дохода свыше 30,000
+            }
+        }
+    }
+    public class FixedRateTaxCalculationStrategy : TaxCalculationStrategy
+    {
+        private decimal fixedRate;
+        public FixedRateTaxCalculationStrategy(decimal fixedRate)
+        {
+            this.fixedRate = fixedRate;
+        }
+        // Реализация метода CalculateTax() для фиксированной ставки налога
+        public override decimal CalculateTax(decimal income)
+        {
+            return income * fixedRate; // Расчет налога по фиксированной ставке
+        }
+    }
+    public class TaxCalculator
+    {
+        private TaxCalculationStrategy taxCalculationStrategy;
+        public void SetTaxCalculationStrategy(TaxCalculationStrategy taxCalculationStrategy)
+        {
+            this.taxCalculationStrategy = taxCalculationStrategy;
+        }
+        public decimal CalculateTax(decimal income)
+        {
+            if (taxCalculationStrategy == null)
+            {
+                throw new InvalidOperationException("Стратегия расчёта налогов не установлена!");
+            }
+            return taxCalculationStrategy.CalculateTax(income); // Выполнение расчета налога 
+        }
+    }
+    // --------------------------------------------------------------------------
+    public abstract class Observer
+    {
+        public abstract void Update(string message);
+    }
+    public class Logger : Observer
+    {
+        public override void Update(string message)
+        {
+            Console.WriteLine($"[LOG] {message}");
+        }
+    }
+    public class EmailNotifier : Observer
+    {
+        public override void Update(string message)
+        {
+            Console.WriteLine($"[EMAIL NOTIFICATION] {message}");
+        }
+    }
+    public class Server
+    {
+        private List<Observer> observers = new List<Observer>();
+        public void Attach(Observer observer)
+        {
+            observers.Add(observer);
+        }
+        public void Detach(Observer observer)
+        {
+            observers.Remove(observer);
+        }
+        public void Notify(string message)
+        {
+            foreach (var observer in observers)
+            {
+                observer.Update(message);
+            }
+        }
+    }
+    // --------------------------------------------------------------------------
+    public abstract class DocumentState
+    {
+        public abstract void Open();
+        public abstract void Save();
+        public abstract void Close();
+        public abstract void Print();
+    }
+    public class NewState : DocumentState
+    {
+        public override void Open()
+        {
+            Console.WriteLine("Документ открыт!");
+        }
+        public override void Save()
+        {
+            Console.WriteLine("Документ должен быть сначала открыт перед сохранением!");
+        }
+        public override void Close()
+        {
+            Console.WriteLine("Новый документ не требует закрытия!");
+        }
+        public override void Print()
+        {
+            Console.WriteLine("Невозможно распечатать новый документ!");
+        }
+    }
+    public class OpenState : DocumentState
+    {
+        public override void Open()
+        {
+            Console.WriteLine("Документ уже открыт!");
+        }
+        public override void Save()
+        {
+            Console.WriteLine("Документ сохранён!");
+        }
+        public override void Close()
+        {
+            Console.WriteLine("Документ закрыт!");
+        }
+        public override void Print()
+        {
+            Console.WriteLine("Документ распечатан!");
+        }
+    }
+    public class SavedState : DocumentState
+    {
+        public override void Open()
+        {
+            Console.WriteLine("Документ открыт из сохраненного состояния!");
+        }
+        public override void Save()
+        {
+            Console.WriteLine("Документ уже сохранён!");
+        }
+        public override void Close()
+        {
+            Console.WriteLine("Документ закрыт!");
+        }
+        public override void Print()
+        {
+            Console.WriteLine("Документ распечатан!");
+        }
+    }
+    public class ModifiedSate : DocumentState
+    {
+        public override void Open()
+        {
+            Console.WriteLine("Документ открыт с несохраненными изменениями!");
+        }
+        public override void Save()
+        {
+            Console.WriteLine("Изменения сохранены!");
+        }
+        public override void Close()
+        {
+            Console.WriteLine("Документ закрыт. Изменения не были сохранены!");
+        }
+        public override void Print()
+        {
+            Console.WriteLine("Документ распечатан с учётом изменений!");
+        }
+    }
+    public class Document
+    {
+        private DocumentState state;
+        public Document()
+        {
+            state = new NewState();
+        }
+        public void SetState(DocumentState state)
+        {
+            this.state = state;
+        }
+        public void Open()
+        {
+            state.Open();
+        }
+        public void Save()
+        {
+            state.Save();
+        }
+        public void Close()
+        {
+            state.Close();
+        }
+        public void Print()
+        {
+            state.Print();
+        }
 
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            // Создаем устройства
             Light light = new Light();
             AirConditioner airConditioner = new AirConditioner();
             TV tv = new TV();
-
-            // Создаем команды
-            Command lightOn = new LightOnCommand(light);
-            Command lightOff = new LightOffCommand(light);
-            Command airConditionerOn = new AirConditionerOnCommand(airConditioner);
-            Command airConditionerOff = new AirConditionerOffCommand(airConditioner);
-            Command tvOn = new TVOnCommand(tv);
-            Command tvOff = new TVOffCommand(tv);
-
-            // Создаем пульт управления
             RemoteControl remote = new RemoteControl();
+            Console.WriteLine("Выберите устройство: (1) Свет, (2) Кондицинер, (3) Телевизор");
+            int choiceDevice = Convert.ToInt32(Console.ReadLine());
+            switch (choiceDevice)
+            {
+                case 1:
+                    Console.WriteLine("Вы выбрали свет");
+                    if (choiceDevice == 1)
+                    {
+                        Console.WriteLine("Выберите действие: (1) включить свет, (2) выключить свет, (3) параметры света");
+                        int choiceLight = Convert.ToInt32(Console.ReadLine());
+                        switch (choiceLight)
+                        {
+                            case 1:
+                                Command lightOn = new LightOnCommand(light);
+                                remote.SetCommand(lightOn);
+                                remote.PressButton();
+                                break;
+                            case 2:
+                                Command lightOff = new LightOffCommand(light);
+                                remote.SetCommand(lightOff);
+                                remote.PressButton();
+                                break;
+                            case 3:
+                                Command lightSettings = new LightSettingsCommand(light);
+                                remote.SetCommand(lightSettings);
+                                remote.PressButton();
+                                break;
+                        }
+                    }
+                    break;
+                case 2:
+                    Console.WriteLine("Вы выбрали кондицинер");
+                    if (choiceDevice == 2)
+                    {
+                        Console.WriteLine("Выберите действие: (1) включить кондицинер, (2) выключить кондицинер, (3) параметры кондицинера");
+                        int choiceAirConditioner = Convert.ToInt32(Console.ReadLine());
+                        switch (choiceAirConditioner)
+                        {
+                            case 1:
+                                Command airConditionerOn = new AirConditionerOnCommand(airConditioner);
+                                remote.SetCommand(airConditionerOn);
+                                remote.PressButton();
+                                break;
+                            case 2:
+                                Command airConditionerOff = new AirConditionerOffCommand(airConditioner);
+                                remote.SetCommand(airConditionerOff);
+                                remote.PressButton();
+                                break;
+                            case 3:
+                                Command airConditionerSettings = new AirConditionerSettingsCommand(airConditioner);
+                                remote.SetCommand(airConditionerSettings);
+                                remote.PressButton();
+                                break;
+                        }
+                    }
+                    break;
+                case 3:
+                    Console.WriteLine("Вы выбрали телевизор");
+                    if (choiceDevice == 3)
+                    {
+                        Console.WriteLine("Выберите действие: (1) включить телевизор, (2) выключить телевизор, (3) параметры телевизора");
+                        int choiceTV = Convert.ToInt32(Console.ReadLine());
+                        switch (choiceTV)
+                        {
+                            case 1:
+                                Command tvOn = new TVOnCommand(tv);
+                                remote.SetCommand(tvOn);
+                                remote.PressButton();
+                                break;
+                            case 2:
+                                Command tvOff = new TVSettingsCommand(tv);
+                                remote.SetCommand(tvOff);
+                                remote.PressButton();
+                                break;
+                            case 3:
+                                Command tvSettings = new TVSettingsCommand(tv);
+                                remote.SetCommand(tvSettings);
+                                remote.PressButton();
+                                break;
+                        }
+                    }
+                    break;
+            }
+            // --------------------------------------------------------------------------
 
-            // Включаем свет
-            remote.SetCommand(lightOn);
-            remote.PressButton();
+            TaxCalculator taxCalculator = new TaxCalculator();
+            Console.WriteLine("Введите ваш доход: ");
+            decimal income = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Выберите систему налогообложения: (1) Прогрессивная ставка, (2) Фиксированная ставка");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    taxCalculator.SetTaxCalculationStrategy(new ProgressiveTaxCalculationStrategy());
+                    break;
+                case 2:
+                    Console.WriteLine("Введите фиксированную ставку (например, 0.15 для 15%): ");
+                    decimal fixedRate = Convert.ToDecimal(Console.ReadLine());
+                    taxCalculator.SetTaxCalculationStrategy(new FixedRateTaxCalculationStrategy(fixedRate));
+                    break;
+                default:
+                    Console.WriteLine("Некорректный выбор.");
+                    return;
+            }
+            decimal tax = taxCalculator.CalculateTax(income); // Рассчитываем налог
+            Console.WriteLine($"Сумма налога: {tax}");
+            // --------------------------------------------------------------------------
 
-            // Выключаем свет
-            remote.SetCommand(lightOff);
-            remote.PressButton();
+            Server server = new Server(); // Создаем экземпляр сервера
+            Logger logger = new Logger(); // Создаем экземпляр логгера
+            EmailNotifier emailNotifier = new EmailNotifier(); // Создаем экземпляр EmailNotifer
 
-            // Включаем кондиционер
-            remote.SetCommand(airConditionerOn);
-            remote.PressButton();
+            server.Attach(logger); // Добавляем логгер в список подписчиков
+            server.Attach(emailNotifier); // Добавляем EmailNotifier в список подписчиков
 
-            // Выключаем кондиционер
-            remote.SetCommand(airConditionerOff);
-            remote.PressButton();
+            server.Notify("Работает");
+            server.Notify("Ошибка 500");
+            server.Notify("Восстановлен");
+            // --------------------------------------------------------------------------
 
-            // Включаем телевизор
-            remote.SetCommand(tvOn);
-            remote.PressButton();
+            Document doc = new Document(); // Создаем экземпляр документа
 
-            // Выключаем телевизор
-            remote.SetCommand(tvOff);
-            remote.PressButton();
+            doc.Open();  // Открываем новый документ
+            doc.Save();  // Сохраняем документ 
+            doc.Print(); // Печатаем документ 
+
+            doc.Close(); // Закрываем документ 
+
+            doc.Open();  // Открываем документ снова 
+            doc.Save();  // Сохраняем его снова 
+
+
+
+
             Console.ReadLine();
         }
     }
